d5d807b1446ad8eced2d9ab73b4c592fa25bb702
GriffinDev <griffin.kode@outlook.com>
2024-11-03 22:43:56 +0530

    ways to print 'Hello World..!'

---
+++ 01_js_intro/01_printHelloWorld.js
@@ -0,0 +1,16 @@
+//?> print "Hello World..!" on the screen.
+
+//> 3ways to print "Hello World..!"
+    //> console.log()
+    //> alert()
+    //> document.write()
+
+//. console.log(), used in debugging the code.
+console.log('Hello World..!');
+
+//. alert(), method displays an alertbox on the currentWindow with the specified message.
+alert('Hello World..!')
+
+//. document.write(), used to print the content in the HTML doc.
+document.write('Hello World..!')
+
